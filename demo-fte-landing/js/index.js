2bb258d79b604e9e40a83f81ce1fb6138f8518ba
tao2.guo <tao2.guo@symbio.com>
2013-05-24 16:29:59 +0800

    theme - lading demo improvement

--- demo-fte-landing/js/index.js
+++ demo-fte-landing/js/index.js
@@ -1,119 +1,134 @@
 (function(){
-	$.fn.extend(
-		/* ribbon */
-		{
+	$.fn.extend({
+			/*set image to real wdth, .e.g. prevent the huge image to zoom in*/
+			setRealWidth:function(){
+				return this.each(function(){
+					//get real width
+					var screenImage = $(this).find('.ribbon-stuff');
+					var theImage = new Image();
+					theImage.src = screenImage.attr('src');
+					var width = theImage.width;
+					//prepare layout
+					var copy = $(this).find('.ribbon-stuff').clone();
+					$(this).find('.ribbon-stuff').remove();
+					$(this).find('.ribbon-inner').css('width', 2*width + 'px').prepend('<div class="ribbon-frame f-a"></div><div class="ribbon-frame f-b"></div>');
+					$(this).find('.ribbon-frame').css('width', width + 'px').append(copy);
+				});
+			},
+		
+			/* ribbon */
 			ribbon:function(options){
 				var defaults = {
 					direction : 'left',
 					distance  : 1
 				};
 				var options = $.extend(defaults, options);
-				
 				return this.each(function(){
-					//prepare layout
-					var copy = $(this).find('.ribbon-stuff').clone();
-					$(this).find('.ribbon-stuff').remove();
-					$(this).find('.ribbon-inner').prepend('<div class="ribbon-frame f-a"></div><div class="ribbon-frame f-b"></div>');
-					$(this).find('.ribbon-frame').append(copy);
-					var mouseDownFlag = false;
-					var orginalX = 0;
-					var currentX = 0;
+					
 					var self = $(this);
 					var wrapperWidth = self.width();
 					if(options.direction === 'right'){
-						self.find('.ribbon-inner').css('left', -(2*($('.ribbon-frame').width()) - wrapperWidth) + 'px');
+						self.find('.ribbon-inner').css('left', -(2*(self.find('.ribbon-frame').width()) - wrapperWidth) + 'px');
 					}else{
 						self.find('.ribbon-inner').css('left','0px');
 					}
 					
 					//set animation
 					function frameMove(speed,direction){
-						
-						if(direction === 'left'){
+						if(self.hasClass('active')){
 							var orginalLeft = parseInt(self.find('.ribbon-inner').css('left'),10);
-							self.find('.ribbon-inner').css('left', orginalLeft - speed + 'px');
-							var deviation = parseInt(self.find('.ribbon-inner').css('left'),10) + (2*($('.ribbon-frame').width()) - wrapperWidth);
-							if(deviation<=speed && deviation>=-speed){
-								self.find('.ribbon-inner').css('left', -($('.ribbon-frame').width() - wrapperWidth) + 'px' );
+							if(direction === 'left'){
+								self.find('.ribbon-inner').css('left', orginalLeft - speed + 'px');
+								var deviation = parseInt(self.find('.ribbon-inner').css('left'),10) + (2*(self.find('.ribbon-frame').width()) - wrapperWidth);
+								if(deviation<=speed && deviation>=-speed){
+									self.find('.ribbon-inner').css('left', -(self.find('.ribbon-frame').width() - wrapperWidth) + 'px' );
+								}
 							}
-						}
-						if(direction === 'right'){
-							var orginalLeft = parseInt(self.find('.ribbon-inner').css('left'),10);
-							self.find('.ribbon-inner').css('left', orginalLeft + speed + 'px');
-							var deviation = parseInt(self.find('.ribbon-inner').css('left'),10);
-							if(deviation<=speed && deviation>=-speed){
-								self.find('.ribbon-inner').css('left', - $('.ribbon-frame').width() + 'px');
+							if(direction === 'right'){
+								self.find('.ribbon-inner').css('left', orginalLeft + speed + 'px');
+								var deviation = parseInt(self.find('.ribbon-inner').css('left'),10);
+								if(deviation<=speed && deviation>=-speed){
+									self.find('.ribbon-inner').css('left', - self.find('.ribbon-frame').width() + 'px');
+								}
 							}
 						}
 					}
-					var frameAnimation = setInterval(function(){frameMove(options.distance,options.direction)},1);
 					
-					
-					//drag to spped up or speed down
-					self.bind('mousedown', function(event){
-						event.preventDefault(); //this is real important!
-						orginalX = event.clientX;
-						mouseDownFlag = true;
-					});
-					self.bind('mousemove', function(event){
-						//now we know it's a drag but not a click
-						if(mouseDownFlag){
+					var frameAnimation;
+					var clicked = false;
+					function startSlide(){
+						var orginalX = 0;
+						var currentX = 0;
+						//var mouseDownFlag = false;
+						frameAnimation = setInterval(function(){frameMove(options.distance,options.direction)},1);
+						//drag to spped up or speed down and click / dbclick event 
+						self.bind('mousedown', function(event){
+							event.preventDefault(); //this is real important!
+							orginalX = event.clientX;
+						});
+						var mouseUp = 0;
+						var speed = options.distance;
+						var direction = options.direction;
+						self.bind('mouseup',function(event){
 							currentX = event.clientX;
-						}
+							if(currentX - orginalX>80 || currentX - orginalX < -80){
+								//drag
+								clearInterval(frameAnimation);
+								if(currentX - orginalX>80 && direction === 'left'){
+									speed +=1;
+									direction = 'left';
+								}
+								if(currentX - orginalX<-80 && direction === 'right'){
+									speed +=1;
+									direction = 'right';
+								}
+								if(currentX - orginalX<-80 && direction === 'left'){
+									direction = 'right';
+									speed = options.distance;
+								}
+								if(currentX - orginalX>80 && direction === 'right'){
+									direction = 'left';
+									speed = options.distance;
+								}
+								frameAnimation = setInterval(function(){frameMove(speed,direction)},1);
+								mouseUp = 0;
+							}else{
+								mouseUp++;
+								if(mouseUp == 1){
+									setTimeout(function(){
+										if(mouseUp == 1){
+											//single click
+											clicked = true;
+											clearInterval(frameAnimation);
+											speed = options.distance;
+											mouseUp = 0;
+										}else{
+											//double click
+											var left = ($(document).width()-450)/2 + 'px'
+											$('.pop-up').css('left',left).fadeIn(500);
+											$('span.close').bind('click', function(){
+												$('.pop-up').fadeOut(500);
+											})
+											mouseUp = 0;
+										}
+									},200)
+								}
+							}
+						}); 
 						
-					});
-					var mouseUp = 0;
-					var speed = options.distance;
-					var direction = options.direction;
-					self.bind('mouseup',function(event){
-						
-						if(currentX - orginalX>20 || currentX - orginalX < -20){
-							//drag
-							clearInterval(frameAnimation);
-							if(currentX - orginalX>20 && direction === 'left'){
-								speed +=1;
-								direction = 'left';
-							}
-							if(currentX - orginalX<-20 && direction === 'right'){
-								speed +=1;
-								direction = 'right';
-							}
-							if(currentX - orginalX<-20 && direction === 'left'){
-								direction = 'right';
-								speed = options.distance;
-							}
-							if(currentX - orginalX>20 && direction === 'right'){
-								direction = 'left';
-								speed = options.distance;
-							}
-							
-							frameAnimation = setInterval(function(){frameMove(speed,direction)},1);
-							mouseUp = 0;
-						}else{
-							
-							mouseUp++;
-							if(mouseUp == 1){
-								setTimeout(function(){
-									if(mouseUp == 1){
-										//single click
-										clearInterval(frameAnimation);
-										speed = options.distance;
-										mouseUp = 0;
-									}else{
-										//double click
-										var left = ($(document).width()-450)/2 + 'px'
-										$('.pop-up').css('left',left).fadeIn(500);
-										$('span.close').bind('click', function(){
-											$('.pop-up').fadeOut(500);
-										})
-										mouseUp = 0;
-									}
-								},200)
-							}
+					}
+					startSlide();
+					//add mouseover function
+					self.bind('mouseover', function(){
+						if(!$(this).hasClass('active')){
+							$('.ribbon.active').removeClass('active');
+							$(this).addClass('active');
+							if(clicked){
+								clearInterval(frameAnimation);
+								frameAnimation = setInterval(function(){frameMove(options.distance,options.direction)},1);
+							}	
 						}
-					}); 
-					
-					
+					})	
 				});
 			}
 
@@ -121,8 +136,19 @@
 	);
 })(jQuery);
 $(document).ready(function(){
-	$('.ribbon').ribbon({
+	$('.ribbon').setRealWidth();
+	$('.ribbon').eq(0).addClass('active');
+	$('.ribbon').eq(0).ribbon({
+		direction : 'left',
+		distance  : 1
+	});
+	$('.ribbon').eq(1).ribbon({
 		direction : 'right',
 		distance  : 1
 	});
+	$('.ribbon').eq(2).ribbon({
+		direction : 'left',
+		distance  : 2
+	});
+	
 });
