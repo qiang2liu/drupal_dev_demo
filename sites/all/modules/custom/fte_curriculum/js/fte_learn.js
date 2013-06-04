257bbc18deda4f8a4904904bcb364753e0771202
qiang2liu <qiang2.liu@symbio.com>
2013-06-04 16:14:58 +0800

    learn-module-stage demp page

--- sites/all/modules/custom/fte_curriculum/js/fte_learn.js
+++ sites/all/modules/custom/fte_curriculum/js/fte_learn.js
@@ -1,6 +1,61 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+    
+    function onYouTubePlayerReady(playerId) {
+      
+      ytplayer = document.getElementById("myytplayer");
+      if(Drupal.settings.fte_learn_stages_playtime > 0) {
+         ytplayer.seekTo(Drupal.settings.fte_learn_stages_playtime,false);
+      } 
+      
+      ytplayer.playVideo();
+      ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
+      setup_video_status_capture(ytplayer);
+  
+    }
+    
+
+    function onytplayerStateChange(newState) {
+     
+      if (newState == 0) {//end
+        if (Drupal.settings.fte_learn_stages.next_sid != -1) {
+          location.href ="/learn/"+Drupal.settings.fte_learn_stages.next_mid+"/stage/"+Drupal.settings.fte_learn_stages.next_sid;
+        }
+      }
+
+    }
+    
+    
+    var timerId;
+function  setup_video_status_capture(ytplayer) {
+  
+  
+  timerId = setInterval(function() {capture_video_process(ytplayer);},1000);
+}
+    
+    function capture_video_process(ytplayer) {
+
+
+
+
+
+  var playtime = ytplayer.getCurrentTime();
+  setCookie("fte_learn_current_yid",Drupal.settings.fte_learn_stages_yid,30);
+  setCookie("fte_learn_playtime", playtime,30);
+
+  
+}
+
+    function setCookie(c_name,value,expiredays)
+{
+  var exdate=new Date()
+  exdate.setDate(exdate.getDate()+expiredays)
+  document.cookie=c_name+ "=" +escape(value)+
+  ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+}
+    
+  
+  
+  
+  
 
 
---
+++ sites/all/modules/custom/fte_curriculum/templates/fte-learn-module-stages-show.tpl.php
@@ -0,0 +1,186 @@
+<?php
+
+$the_path = drupal_get_path('module', 'fte_curriculum');
+//drupal_add_js($the_path.'/js/fte_learn.js');
+?>
+
+
+
+<div
+
+  style="background: url(<?php print base_path() . drupal_get_path('module', 'fte_curriculum') ?>/images/greenline.png) center center repeat-x ;
+  display: inline-block;
+
+  ">
+
+
+
+<?php
+$i = 1;
+foreach ($data->stages as $row) {
+
+  if ($i == 1 || $i >= count($data->stages)) {
+    $m = '';
+  } else {
+    $m = 'margin: 10px;';
+  }
+
+  if ($i == $data->stagedata->sid) {
+    $the_class = "#00ff00";
+  } else {
+    $the_class = "#fff";
+  }
+  ?>
+
+
+
+    <div style="
+         border:1px solid green;
+         background:<?php print $the_class; ?>;
+         border-radius: 10px;
+         cursor:pointer;
+         width:20px;
+         height:20px;
+         display:inline-block;
+         text-align:center;
+  <?php print $m; ?>
+         "
+         
+         onclick ="change_stage('<?php print $data->stagedata->next_mid;?>',
+           '<?php print $row->sid;?>')"
+         > 
+
+         <?php print $i++; ?> </div>
+         <?php
+       }
+       ?>
+
+
+
+</div>
+
+<?php 
+
+ setcookie('fte_learn_current_yid',$data->stagedata->yid,time()+3600*30);
+ if ($data->stagedata->yid == '') {
+    setcookie('fte_learn_playtime',0,time()+3600*30);
+ } else {
+  setcookie('fte_learn_playtime',$data->stagedata->time,time()+3600*30);
+ } 
+ setcookie('fte_learn_current_mid',$data->stagedata->next_mid,time()+3600*30);
+ setcookie('current_stage_id',$data->stagedata->sid,time()+3600*30);
+  
+  if (!empty($data->stagedata->yid)) {
+    
+ ?>   
+
+<div id="ytapiplayer">
+    You need Flash player 8+ and JavaScript enabled to view this video.
+</div>
+<?php
+      // video stage
+
+
+
+
+
+    drupal_add_js($the_path.'/js/fte_learn.js');
+    drupal_add_js(array('fte_learn_stages' => $data->stagedata), 'setting');
+    drupal_add_js(array('fte_learn_stages_yid' =>$data->stagedata->yid), 'setting');
+    drupal_add_js(array('fte_learn_stages_playtime' =>$data->stagedata->time), 'setting');
+    
+    ?>
+
+<script>
+  
+   var ytplayer;
+    var params = {
+      allowScriptAccess: "always"
+    };
+    var atts = {
+      id: "myytplayer"
+    };
+   
+  
+   $(document).ready(function(){
+     
+      swfobject.embedSWF("http://www.youtube.com/v/"+Drupal.settings.fte_learn_stages_yid
+        +"?enablejsapi=1&playerapiid=ytplayer&version=3&modestbranding=1",
+        "ytapiplayer", "780", "439", "8", null, null, params, atts);
+  
+    
+  
+
+
+
+}
+    
+
+
+); 
+</script>
+
+
+<?php
+    
+  } else {
+?>
+
+<div>
+  <?php print $data->stagedata->content?>
+  <?php
+    if ($data->stagedata->next_sid >= 0) {
+  ?>
+  <br/>
+  <a href =" /learn/<?php print $data->stagedata->next_mid;?>/stage/<?php print $data->stagedata->next_sid;?>"> next stage </a>
+  <?php
+  }
+  ?>
+</div>
+</div>
+
+<?php
+  }
+
+?>
+
+<script>
+  
+   var ytplayer;
+    var params = {
+      allowScriptAccess: "always"
+    };
+    var atts = {
+      id: "myytplayer"
+    };
+   
+  
+   $(document).ready(function(){
+      console.log(atts);
+      swfobject.embedSWF("http://www.youtube.com/v/"+Drupal.settings.fte_learn_stages_yid
+        +"?enablejsapi=1&playerapiid=ytplayer&version=3&modestbranding=1",
+        "ytapiplayer", "780", "439", "8", null, null, params, atts);
+  
+    
+  
+
+
+
+}
+    
+
+
+);   
+  
+  
+  function change_stage(m,s) {
+    location.href="/learn/"+m+"/stage/"+s;
+  }
+  
+  </script>
+  
+  
+  
+  
+  
+  
