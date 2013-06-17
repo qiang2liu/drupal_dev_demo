63eebbc139ba344f3b49bca90c9309884f883c94
tao2.guo <tao2.guo@symbio.com>
2013-06-17 17:35:47 +0800

    vertical

--- demo-fte-landing/js/index.js
+++ demo-fte-landing/js/index.js
@@ -579,19 +579,19 @@ $(window).load(function(){
 	});
 	//add hot spot
 	$('.ribbon').eq(0).hotSpot({
-		url:'hotspot.asp'
+		url:'hotspot.asp?123'
 	});
 	$('.ribbon').eq(1).hotSpot({
-		url:'hotspot1.asp'
+		url:'hotspot1.asp?123'
 	});
 	$('.ribbon').eq(2).hotSpot({
-		url:'hotspot2.asp'
+		url:'hotspot2.asp?123'
 	});
 	$('.ribbon').eq(3).hotSpot({
-		url:'hotspot1.asp'
+		url:'hotspot1.asp?123'
 	});
 	$('.ribbon').eq(4).hotSpot({
-		url:'hotspot2.asp'
+		url:'hotspot2.asp?123'
 	});
 	
 	//popup close function
