7e6ee1324cde73e4f04705a117b1f666023928eb
qiang2liu <qiang2.liu@symbio.com>
2013-06-18 16:25:21 +0800

    dashboard page data

--- sites/all/modules/custom/fte/templates/landing-page.tpl.php
+++ sites/all/modules/custom/fte/templates/landing-page.tpl.php
@@ -1,3 +1,604 @@
 <?php
+global $base_url
 ?>
 <img src ="/images/dashboard_mockup.png"/>
+<div class="container">
+
+  <div class='inspiration'>
+    <div class='title'>Inspiration</div>
+    <div>
+      <span> <a href="<?php print $base_url;?>/node/49"> <img src="<?php print $base_url; ?>/images/mock_dashboard_inspiration_1.jpg"></a></span>
+      <span> <a href="<?php print $base_url;?>/node/50"> <img src="<?php print $base_url; ?>/images/mock_dashboard_inspiration_2.jpg"></a></span>
+    </div>
+  </div>
+  
+  
+  <div class='Badges'>
+    <div class='title'>Badges</div>
+    <div>
+      <img src="<?php print $base_url; ?>/images/mock_dashboard_badges_1.jpg"></span>
+      
+    </div>
+  </div>
+  
+  
+  <div class='points'>
+    <div class='title'>Points</div>
+    <div class="points">
+      8,042
+    </div>
+  </div>
+  
+  
+  <div class='total_time'>
+    <div class='title'>Total time on:</div>
+    <div class="total_time">
+      22:37
+    </div>
+  </div>
+  
+  
+  <div class='community_act'>
+    <div class='title'>Community Activity</div>
+    <div class="content_activity">
+      <table>
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_1.jpg"/>
+          </td>
+          <td>
+            John has uploaded a strategy pager <img src="<?php print $base_url;?>/images/mock_face_10.jpg"/> to the Innovator Project
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_2.jpg"/>
+          </td>
+          <td>
+            Kathleen is now friends with <img src="<?php print $base_url;?>/images/mock_face_9.jpg"/> Marc
+          </td>
+        </tr>
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_3.jpg"/>
+          </td>
+          <td>
+            Taylor is now a member of the inner Kabal
+          </td>
+        </tr>
+        
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_4.jpg"/>
+          </td>
+          <td>
+            Heather just left a comment on Marc's blog post...
+          </td>
+        </tr>
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_5.jpg"/>
+          </td>
+          <td>
+            Gau Tao just posted "<a href="<?php print $base_url;?>/node/47">Why I love Partallax?</a>" to his blog...
+          </td>
+        </tr>
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_6.jpg"/>
+          </td>
+          <td>
+            Lisa just added Marc <img src="<?php print $base_url;?>/images/mock_face_8.jpg"/> as her friend
+          </td>
+        </tr>
+        
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_7.jpg"/>
+          </td>
+          <td>
+            Aryeh just created a Project called: <a href="<?php print $base_url;?>/node/48">Sustainability and Judaism</a>
+          </td>
+        </tr>
+        
+        
+      </table>
+    </div>
+  </div>
+  
+  
+  
+  <div class='showcases'>
+    <div class='title'>Showcases</div>
+    <div class="case">
+      <span> <a href="<?php print $base_url; ?>/node/43"><img src="<?php print $base_url; ?>/images/mock_dashboard_case_1.jpg"></a></span>
+      <span> <a href="<?php print $base_url; ?>/node/44"><img src="<?php print $base_url; ?>/images/mock_dashboard_case_2.jpg"></a></span>
+      <span> <a href="<?php print $base_url; ?>/node/45"><img src="<?php print $base_url; ?>/images/mock_dashboard_case_3.jpg"></a></span>
+      <span> <a href="<?php print $base_url; ?>/node/46"><img src="<?php print $base_url; ?>/images/mock_dashboard_case_4.jpg"></a></span>
+    </div>
+  </div>
+  
+  
+  
+  
+  <div class='facewall' id='facewall_1'>
+    <div class='title'>Facewall</div>
+    <div calss='face_sort_title'>my friends</div>
+    <div class="facelist">
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_1.jpg">
+        </div>
+        <div class="facename">
+
+          John smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_2.jpg">
+        </div>
+        <div class="facename">
+
+          Kathleen smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_3.jpg">
+        </div>
+        <div class="facename">
+
+          Taylor smith
+        </div>
+      </span>  
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_4.jpg">
+        </div>
+        <div class="facename">
+
+          Heather smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_5.jpg">
+        </div>
+        <div class="facename">
+
+          Gau Tao
+        </div>
+      </span>
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_6.jpg">
+        </div>
+        <div class="facename">
+
+          Lisa
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_7.jpg">
+        </div>
+        <div class="facename">
+
+          Aryeh smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_8.jpg">
+        </div>
+        <div class="facename">
+
+          Marc 
+        </div>
+      </span>
+        
+    </div>
+  </div>
+  
+  
+  <div class='facewall' id='facewall_2'>
+    <div class='title'>Facewall</div>
+    <div calss='face_sort_title'>On projects</div>
+    <div class="facelist">
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_2.jpg">
+        </div>
+        <div class="facename">
+
+          John smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_1.jpg">
+        </div>
+        <div class="facename">
+
+          Kathleen smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_4.jpg">
+        </div>
+        <div class="facename">
+
+          Taylor smith
+        </div>
+      </span>  
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_3.jpg">
+        </div>
+        <div class="facename">
+
+          Heather smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_6.jpg">
+        </div>
+        <div class="facename">
+
+          Gau Tao
+        </div>
+      </span>
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_5.jpg">
+        </div>
+        <div class="facename">
+
+          Lisa 
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_8.jpg">
+        </div>
+        <div class="facename">
+
+          Aryeh smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_7.jpg">
+        </div>
+        <div class="facename">
+
+          Marc
+        </div>
+      </span>
+        
+    </div>
+  </div>
+  
+  
+  <div class='facewall' id='facewall_3'>
+    <div class='title'>Facewall</div>
+    <div calss='face_sort_title'>Location</div>
+    <div class="facelist">
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_8.jpg">
+        </div>
+        <div class="facename">
+
+          John smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_3.jpg">
+        </div>
+        <div class="facename">
+
+          Kathleen smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_2.jpg">
+        </div>
+        <div class="facename">
+
+          Taylor smith
+        </div>
+      </span>  
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_4.jpg">
+        </div>
+        <div class="facename">
+
+          Heather smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_7.jpg">
+        </div>
+        <div class="facename">
+
+          Gau Tao
+        </div>
+      </span>
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_6.jpg">
+        </div>
+        <div class="facename">
+
+          Lisa
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_5.jpg">
+        </div>
+        <div class="facename">
+
+          Aryeh smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_1.jpg">
+        </div>
+        <div class="facename">
+
+          Marc
+        </div>
+      </span>
+        
+    </div>
+  </div>
+  
+  
+  <div class='facewall' id='facewall_4'>
+    <div class='title'>Facewall</div>
+    <div calss='face_sort_title'>Interests</div>
+    <div class="facelist">
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_4.jpg">
+        </div>
+        <div class="facename">
+
+          John smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_1.jpg">
+        </div>
+        <div class="facename">
+
+          Kathleen smith
+        </div>
+      </span>  
+      
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_2.jpg">
+        </div>
+        <div class="facename">
+
+          Taylor smith
+        </div>
+      </span>  
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_3.jpg">
+        </div>
+        <div class="facename">
+
+          Heather smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_6.jpg">
+        </div>
+        <div class="facename">
+
+          Gau Tao
+        </div>
+      </span>
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_5.jpg">
+        </div>
+        <div class="facename">
+
+          Lisa 
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_8.jpg">
+        </div>
+        <div class="facename">
+
+          Aryeh smith
+        </div>
+      </span>
+      
+      <span class='faceitem'>
+        <div class="faceimg">
+          <img src="<?php print $base_url; ?>/images/mock_face_7.jpg">
+        </div>
+        <div class="facename">
+
+          Marc
+        </div>
+      </span>
+        
+    </div>
+  </div>
+  
+  
+  
+  
+  <div class='sortby'>
+    <div class='title'>Sort by...</div>
+    <div class="sortitem">
+      <ul>
+        <li>
+          <a href='javascript:void(0);' onclick="sortface(1)"> My Friends</a>
+        </li>
+        <li>
+          <a href='javascript:void(0);' onclick="sortface(2)"> On Projects</a>
+        </li>
+        <li>
+          <a href='javascript:void(0);' onclick="sortface(3)"> Location</a>
+        </li>
+        <li>
+          <a href='javascript:void(0);' onclick="sortface(4)"> Interests</a>
+        </li>
+      </ul>
+      
+      
+    </div>
+  </div>
+  <script>
+     function sortface(index){
+       
+       
+       
+     }
+     
+  </script>
+  
+  
+  
+</div>
+
+
+
+
+
+ <div class='leaderboard'>
+    <div class='title'>Leaderboard</div>
+    <div class="content_leader">
+      <table>
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_11.jpg"/>
+          </td>
+          <td>
+            Brieana Polk-Perez 
+          </td>
+          <td>8,947</td>
+          <td>23:17</td>
+        </tr>
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_12.jpg"/>
+          </td>
+          <td>
+            Mary Donovan
+          </td>
+          <td>8,120</td>
+          <td>22:09</td>
+        </tr>
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_13.jpg"/>
+          </td>
+          <td>
+            Will Berman
+          </td>
+          <td>6,947</td>
+          <td>21:09</td>
+        </tr>
+        
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_14.jpg"/>
+          </td>
+          <td>
+            David Dixon
+          </td>
+          <td>7,318</td>
+          <td>22:44</td>
+        </tr>
+        
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_15.jpg"/>
+          </td>
+          <td>
+            Kim Park
+          </td>
+          <td>4,232</td>
+          <td>19:17</td>
+        </tr>
+       
+        <tr>
+          <td>
+            <img src="<?php print $base_url;?>/images/mock_face_16.jpg"/>
+          </td>
+          <td>
+            Lashonta Jones
+          </td>
+          <td>4,022</td>
+          <td>18:12</td>
+        </tr>
+        
+       
+        
+        
+      </table>
+    </div>
+  </div>
