da24ae4373ac1c02e9ef9d97d56735f4e8574304
tao2.guo <tao2.guo@symbio.com>
2013-06-26 14:13:29 +0800

    ui change

--- sites/all/modules/custom/fte/templates/landing-page.tpl.php
+++ sites/all/modules/custom/fte/templates/landing-page.tpl.php
@@ -45,13 +45,13 @@ global $base_url
         <div class="da-content-box">
         	<div class="da-content">
             	<ul>
-                	<li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"></li>
-                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"></li>
-                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"></li>
-                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"></li>
-                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"></li>
-                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"></li>
-                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"></li>
+                	<li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"> Brianna</li>
+                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"> Guru</li>
+                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"> John</li>
+                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"> Mark</li>
+                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"> Lisa</li>
+                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"> Brianna</li>
+                    <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"> Guru</li>
                     
                 </ul>
             </div>
@@ -71,55 +71,55 @@ global $base_url
     <div class="da-content-box">
     	<div class="da-content">
             <ul>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"></li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"> name</li>
             </ul>
         </div>
         <div class="da-content hide">
             <ul>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"></li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"> name</li>
             </ul>
         </div>
         <div class="da-content hide">
             <ul>
-            	<li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"></li>
+            	<li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"> name</li>
                 
             </ul>
         </div>
         <div class="da-content hide">
             <ul>
-            	<li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"></li>
+            	<li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"> name</li>
 
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"></li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"> name</li>
             </ul>
         </div>
         <div class="da-content hide">
             <ul>
                
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"></li>
-                 <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"></li>
-                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"></li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_05.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_06.png"> name</li>
+                 <li><img src="<?php print $base_url; ?>/images/mock_ico_video_01.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_02.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_03.png"> name</li>
+                <li><img src="<?php print $base_url; ?>/images/mock_ico_video_04.png"> name</li>
             </ul>
         </div>
     </div>
