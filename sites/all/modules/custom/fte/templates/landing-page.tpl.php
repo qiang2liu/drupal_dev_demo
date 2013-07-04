467ed451d753e08ceb1a53b36df2a427a2812688
yangru <yangru@ymail.com>
2013-07-04 12:59:46 +0800

    FTE-61

--- sites/all/modules/custom/fte/templates/landing-page.tpl.php
+++ sites/all/modules/custom/fte/templates/landing-page.tpl.php
@@ -16,6 +16,7 @@ function _get_videos_list() {
 			$link = field_get_items('node', $node, 'field_video_link');
 			$screenshot = field_get_items('node', $node, 'field_video_screenshot');
 			$renderable_array[$tid][] = array(
+			  'nid'=>$row->nid,
 			  'link'=>$link[0]['safe_value'],
 			  'screenshot'=>file_create_url($screenshot[0]['uri']),
 			);
@@ -38,7 +39,8 @@ $videos_list = _get_videos_list();
         	  <div class="da-content">
             	<ul class="img-list ">
 								<?php foreach($videos_list['3'] as $videoinfo):?>
-                	<li><a href="<?php print_r($videoinfo['link']); ?>" target="_blank"><img src="<?php print_r($videoinfo['screenshot']); ?>"></a></li>
+									<?php $nid = $videoinfo['nid']; ?>
+                	<li><a class="myytplayer" id="myytplayer<?php echo $nid;?>" href="javascript:playVideo(<?php echo $nid;?>, '<?php print($videoinfo['link'])?>', '<?php echo $videoinfo['screenshot'];?>');" target="_blank"><img src="<?php print($videoinfo['screenshot']); ?>"></a></li>
 								<?php endforeach;?>
               </ul>
             </div>
@@ -47,7 +49,7 @@ $videos_list = _get_videos_list();
             <div class="da-content hide">
             	<ul class="img-list">
 								<?php foreach($videos_list['4'] as $videoinfo):?>
-                	<li><a href="<?php print_r($videoinfo['link']); ?>" target="_blank"><img src="<?php print_r($videoinfo['screenshot']); ?>"></a></li>
+                	<li><a class="myytplayer" id="myytplayer<?php echo $nid;?>" href="javascript:playVideo(<?php echo $nid;?>, '<?php print($videoinfo['link'])?>', '<?php echo $videoinfo['screenshot'];?>');" target="_blank"><img src="<?php print($videoinfo['screenshot']); ?>"></a></li>
 								<?php endforeach;?>
                 </ul>
             </div>
@@ -144,7 +146,29 @@ $videos_list = _get_videos_list();
         </div>
     </div>
 </div>
-
+<script>
+var screenshotInfo = [];
+function playVideo(nid, url, screenshot) {
+	var ytplayer = document.getElementById("myytplayer"+nid);
+  //ytplayer.innerHTML = '<iframe width="202" height="135" src="'+url+'?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>';
+	ytplayer.innerHTML = '<object width="202" height="135"><param name="movie" value="'+url+'?version=3&amp;autoplay=1&amp;rel=0"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="autoplay" value="true"><embed src="'+url+'?version=3&amp;autoplay=1&amp;rel=0" type="application/x-shockwave-flash" width="202" height="135" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
+	/*var params = { allowScriptAccess: "always" };
+	var atts = { id: "myytplayer"+nid };
+	swfobject.embedSWF(url+"?enablejsapi=1&playerapiid=ytplayer"+nid+"&version=3",
+		"ytapiplayer", "202", "135", "8", null, null, params, atts);*/
+  if(screenshotInfo) {
+	  var oldplayer = document.getElementById("myytplayer"+screenshotInfo[0]);
+		if(oldplayer) oldplayer.innerHTML = '<img src="'+ screenshotInfo[1] +'"/>';
+	}
+	screenshotInfo = [nid, screenshot];
+}
+/*function onYouTubePlayerReady(playerId) {
+	var ytplayer = document.getElementById(playerId);
+	if (ytplayer) {
+    ytplayer.playVideo();
+  }
+}*/
+</script>
 
 
 
