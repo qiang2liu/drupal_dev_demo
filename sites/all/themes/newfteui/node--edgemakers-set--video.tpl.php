653abd2563fe5aefc0f711f41fcf78d10b82017b
yangru <yangru@ymail.com>
2013-07-18 15:04:50 +0800

    FTE-92

--- sites/all/themes/newfteui/node--edgemakers-set--video.tpl.php
+++ sites/all/themes/newfteui/node--edgemakers-set--video.tpl.php
@@ -1,8 +1,15 @@
 <?php
 //title: $title
 //url: 
-      $urls = field_get_items('node', $node, 'field_set_url');
-      $url = $urls && count($urls) > 0 ? $urls[0]['url'] : '';
+$urls = field_get_items('node', $node, 'field_set_url');
+$url = $urls && count($urls) > 0 ? $urls[0]['url'] : '';
+$ytid = youtube_parser($url);
+function youtube_parser($url) {
+	preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $url, $matches);
+	if(is_array($matches) && count($matches) > 0) 
+		return $matches[0];
+	return false;
+}
 ?>
 <style>
 #content div.tabs {
@@ -26,6 +33,9 @@
       hide($content['comments']);
       hide($content['links']);
     ?>
+    <div id="yt">
+    You need Flash player 8+ and JavaScript enabled to view this video.
+    </div>
   </div>
 
   <div class="clearfix">
@@ -37,3 +47,16 @@
   </div>
 
 </div>
+<script>
+(function($){
+  $(document).ready(function(){
+    loadVideo('<?php echo $ytid;?>');
+	});
+})(jQuery);
+function loadVideo(videoid) {
+	var params = { allowScriptAccess: "always" };
+	var atts = { id: "myytplayer" };
+	swfobject.embedSWF("http://www.youtube.com/v/"+videoid+"?enablejsapi=1&playerapiid=playerapi&version=3",
+		"yt", "640", "390", "8", null, null, params, atts);
+}
+</script>
