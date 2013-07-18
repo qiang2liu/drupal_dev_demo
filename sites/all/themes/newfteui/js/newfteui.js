673a914b9272cccbe0a3e4b121a3280160c8db12
tao2.guo <tao2.guo@symbio.com>
2013-07-18 17:45:32 +0800

    new UI

--- sites/all/themes/newfteui/js/newfteui.js
+++ sites/all/themes/newfteui/js/newfteui.js
@@ -19,9 +19,18 @@
 						}
 					);
 					self.find('.indicator span').bind('click', function(){
+						$('.pane').removeClass('show');
+						$('div').not(self).not('.toolbar-handler').not('.toolbar-box').removeClass('active');
 						self.toggleClass('active');
 					})
 				});
+			},
+			toggleToolbar: function(){
+				$('.toolbar-handler, .toolbar-box').toggleClass('active');
+			},
+			removeAllActive : function(){
+				$('div').not('.toolbar-handler').not('.toolbar-box').not(this).removeClass('active');
+				$(this).addClass('active');
 			}
 		});
 	})(jQuery);
@@ -29,5 +38,29 @@
 	//load
 	$(document).ready(function(){
 		$('.community').setCommunity();
+		$('.toolbar-handler').bind('click', $.fn.toggleToolbar);
+		$('.stage-selector-handler>em').hover(
+			function(){
+				$(this).addClass('flash');
+				$('.stage-selector-handler>span').addClass('flash');
+				$('.stage-box').addClass('flash');
+			},
+			function(){
+				$(this).removeClass('flash');
+				$('.stage-selector-handler>span').removeClass('flash');
+				$('.stage-box').removeClass('flash');
+			}
+		);
+		$('.stage-selector').bind('click', function(){
+			$('.pane').removeClass('show');
+			$.fn.removeAllActive.call(this);
+			
+		});
+		$('.pane-handler').bind('click',function(){
+			$.fn.removeAllActive.call(this);
+			var id = $(this).attr('data-aim');
+			$('.pane').not('#'+ id).removeClass('show');
+			$('#'+ id).toggleClass('show');
+		})
 	});
 })(jQuery);
--- sites/all/themes/newfteui/page.tpl.php
+++ sites/all/themes/newfteui/page.tpl.php
@@ -8,59 +8,77 @@
         <div class="indicator"><span></span></div>
       </div>       
 	<?php endif; ?>
-
-	<!-- /tool bar -->
-    <!--<?php if ($page['tool_bar']): ?>-->
-      <div class="tool-bar">
-      	<div class="toolbar-handler"></div>
-      	<div class="toolbar-box">
-      		<div class="toolbar-item title">
-      			<h4>Toolbar</h4>
-      		</div>
-      		<div class="toolbar-item upload">
-      			<h4>Upload</h4>
-      		</div>
-      		<div class="toolbar-item add-an-idea">
-      			<h4>Add an Idea</h4>
-      		</div>
-      		<div class="toolbar-item murals">
-      			<h4>Murals</h4>
-      		</div>
-      		<div class="toolbar-item media">
-      			<h4 class="has-child">Media<em></em></h4>
-      			<ul>
-      				<li class="media-video">Video</li>
-      				<li class="media-image">Images</li>
-      				<li class="media-audio">Audio</li>
-      				<li class="media-docs">Docs</li>
-      			</ul>
-      		</div>
-      		<div class="toolbar-item social">
-      			<span class="social-twitter"></span>
-      			<span class="social-facebook"></span>
-      			<span class="social-gmail"></span>
-      		</div>
-      		<div class="toolbar-item tour-guides">
-      			<h4>Tour Guides</h4>
-      			<p>images here</p>
-      		</div>
-      	</div>
+	
+	<!-- /tool bar and stage selector -->
+   
+      <div class="toolbar-stage">
+      	<div class="tool-bar">
+      		<!-- {{{toolbar start-->
+	      	<div class="toolbar-handler"></div>
+	      	<div class="toolbar-box">
+	      		<!--<div class="toolbar-item title">
+	      			<h4>Toolbar</h4>
+	      		</div>
+	      		<div class="toolbar-item upload">
+	      			<h4>Upload</h4>
+	      		</div>
+	      		<div class="toolbar-item add-an-idea">
+	      			<h4>Add an Idea</h4>
+	      		</div>
+	      		<div class="toolbar-item murals">
+	      			<h4>Murals</h4>
+	      		</div>
+	      		<div class="toolbar-item media">
+	      			<h4 class="has-child">Media<em></em></h4>
+	      			<ul>
+	      				<li class="media-video">Video</li>
+	      				<li class="media-image">Images</li>
+	      				<li class="media-audio">Audio</li>
+	      				<li class="media-docs">Docs</li>
+	      			</ul>
+	      		</div>
+	      		<div class="toolbar-item social">
+	      			<span class="social-twitter"></span>
+	      			<span class="social-facebook"></span>
+	      			<span class="social-gmail"></span>
+	      		</div>
+	      		<div class="toolbar-item tour-guides">
+	      			<h4>Tour Guides</h4>
+	      			<p>images here</p>
+	      		</div>-->
+	      	</div>
+	      	<!-- }}} toolbar end -->
+	      	
+	      	<!-- {{{ Stage Selector start -->
+	      	<div class="stage-selector">
+	      		<div class="stage-selector-inner">
+		      		<div class="stage-selector-handler"><em></em><span>Stage Selector</span></div>
+		      		<div class="stage-box"></div>
+		      	</div>
+	      	</div>
+	      	<!-- }}} Stage Selector end -->
+	      </div>
         <!--<?php print render($page['tool_bar']); ?>-->
       </div> 
-	<!--<?php endif; ?>-->
+	
 	
 	<!-- /main content -->
       <div class="main-content">
-        <?php print render($page['content']); ?>
+        <!--<?php print render($page['content']); ?>-->
       </div> 
 	
 	
 	<!-- /sliding panes -->
-    <?php if ($page['sliding_panes']): ?>
       <div class="sliding-panes">
+      	<div class="studio-handler pane-handler" data-aim="studio"></div>
+      	<div class="chanllenge-handler pane-handler" data-aim="challenge"></div>
+      	<div class="competitions-handler pane-handler"  data-aim="competitions"></div>
         <?php print render($page['sliding_panes']); ?>
-      </div> 
-	<?php endif; ?>
+      </div>
+      <div class="pane" id="studio"></div>
+      <div class="pane" id="challenge"></div>
+      <div class="pane" id="competitions"></div>
+	
 	
 	
 
