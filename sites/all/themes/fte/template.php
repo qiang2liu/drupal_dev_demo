d1eb109fe013afcf405ae7598994d61cbf1e9d3c
yangru <yangru@ymail.com>
2013-07-18 12:17:21 +0800

    FTE-92

--- sites/all/themes/fte/template.php
+++ sites/all/themes/fte/template.php
@@ -112,6 +112,17 @@ function fte_preprocess_node(&$variables) {
   if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
     $variables['classes_array'][] = 'node-full';
   }
+  $node = $variables['node'];
+  if($node->type == 'edgemakers_set') {
+    require_once drupal_get_path('module', 'edgemakers_set').'/edgemakers_set.pages.inc';
+    $terms = _edgemakers_set_get_terms();
+    $types = field_get_items('node', $node, 'field_set_type');
+    $type = $types && count($types) > 0 ? $terms[$types[0]['tid']] : '';
+    if($type == 'Inspiration' || $type == 'Showcase')
+      $type = 'video';
+    if($type == 'Project' || $type == 'Activity' || $type == 'video')
+      $variables['theme_hook_suggestions'][] = 'node__edgemakers_set__'.strtolower($type); 
+  }
 }
 
 /**
