f0e7209c45f3219617b8fb71f24f0c4084acf1c4
tao2.guo <tao2.guo@symbio.com>
2013-07-18 18:02:26 +0800

    new UI

--- sites/all/themes/newfteui/page.tpl.php
+++ sites/all/themes/newfteui/page.tpl.php
@@ -2,13 +2,13 @@
 ?>
 
     <!-- /community -->
-    <?php if ($page['community']): ?>
+    
       <div class="community">
       	<h2>Community</h2>
         <div class="inner community-inner"><p>This is the sample date of the Community.</p></div>
         <div class="indicator"><span></span></div>
       </div>       
-	<?php endif; ?>
+	
 	
 	<!-- /tool bar and stage selector -->
    
