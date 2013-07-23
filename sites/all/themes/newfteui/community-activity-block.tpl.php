a62b1da41d25a7f0f1c3496b824b3a7bdef48e82
yangru <yangru@ymail.com>
2013-07-23 15:26:49 +0800

    FTE-129

---
+++ sites/all/themes/newfteui/community-activity-block.tpl.php
@@ -0,0 +1,34 @@
+<?php
+if(count($contents)>0) {
+  $items = array();
+  for($i = 0; $i < $num; $i ++) {
+    $item = '';
+    $content = $contents[$i];
+    $variables = array(
+      'account' => $content,
+    );
+    $userpic = theme('user_picture', $variables);
+    $variables = array(
+      'account' => $content, 
+    );
+    $username = theme('username', $variables);
+    $date = format_interval(time()-$content->created) . ' ago';
+    $item .= $userpic.'<div class="cont">';
+    if($content->cid) {
+      //comment
+      $title = l($content->title, 'node/'.$content->nid, array('fragment' => 'comment-'.$comment->cid));
+      $item .= '<div class="activity">'.$username.' Submitted a commnet: '.$title.'</div>';
+    } else {
+      //node
+      $title = l($content->title, 'node/'.$content->nid);
+      $type = $content->type;
+      $item .= '<div class="activity">'.$username.' Submitted a '.$type.': '.$title.'</div>';
+    }
+    $item .= '<div class="time">'.$date.'</div></div>';
+    $items[] = $item;
+  }
+  echo theme('item_list', array('items' => $items, 'title'=>'Community Activity'));
+} else {
+  echo t("No Activity.");
+}
+?>
