183b56c2e0caee27b9996887ed111bb60fc681cb
tao2.guo <tao2.guo@symbio.com>
2013-08-06 09:55:33 +0800

    improvement of fte-200

--- sites/all/themes/newfteui/js/newfteui.js
+++ sites/all/themes/newfteui/js/newfteui.js
@@ -4,66 +4,28 @@
 		$.fn.extend({
 			setCommunity : function(options){
 				var defaults = {
-					topDistance: '-790px'
+					topDistance: '-700px'
 				}
 				var options = $.extend(defaults,options);
 				return this.each(function(){
 					var self = $(this);
-					
-					/*self.find('.indicator span').hover(
-						function(){
-							self.addClass('flash');
-						},
-						function(){
-							self.removeClass('flash');
-						}
-					);*/
 					self.find('.indicator span').bind('click', function(){
-						
 						$('div').not(self).not('.toolbar-handler').not('.toolbar-box').removeClass('active');
 						self.toggleClass('active');
-						$('.pane-handler').css('width','').removeClass('current');
-						$('.pane').removeClass('show').css('right','-760px');
+						$('.pane').removeClass('show');
 					})
 				});
 			},
-			makePaneNormal: function(){
-				$('.pane-handler').css('width','').removeClass('current');
-				$('.pane').removeClass('show').css('right','-760px');
-			},
 			setPositionofElements : function(){
 				var space = ($(window).width() - 760)/2;
-				// set
+				// Desitination
 				$('.main-content').css({
 					'left': space + 'px'
-				});
-				
-				//pane
-				
-				$('.pane-handler').bind('click',function(){
-					$('.pane-handler').not($(this)).removeClass('current').css('width','');
-					$(this).toggleClass('current')
-					
-					var id = $(this).attr('data-aim');
-					$('.pane').not('#'+ id).removeClass('show').css('right','');
-					
-					if($('#'+ id).hasClass('show')){
-						$('#'+ id).removeClass('show').css('right','');
-					}else{
-						$('#'+ id).css('right','').addClass('show').css('right', space + 'px' );
-					}
-				});
-				$('.pane-handler').hover(
-					function(){
-						$(this).css('width',space+'px');
-					},
-					function(){
-						if(!$(this).hasClass('active')){
-							$(this).css('width','');
-						}
-						
-					}
-				);
+				});	
+			},
+			makePaneNormal: function(){
+				$('.pane-handler').removeClass('active');
+				$('.pane').removeClass('show');
 			},
 			toggleToolbar: function(){
 				$('.toolbar-handler, .toolbar-box').toggleClass('active');
@@ -81,13 +43,20 @@
 		//set position
 		$.fn.setPositionofElements();
 		$('.community').setCommunity();
+		
+		//when window resize, we use this again
+		$(window).resize(function(){
+			$.fn.setPositionofElements();
+		});
+		
+		//toolbar
 		$('.toolbar-handler').bind('click', $.fn.toggleToolbar);
 		
+		//stage selector
 		$('.stage-selector-handler>em').bind('click', function(){
 			$.fn.removeAllActive.call($('.stage-selector'));
 			$.fn.makePaneNormal();
 		});
-		
 		
 		//user-profile
 		$('.user-profile-inner').bind('click', function(){
@@ -95,8 +64,17 @@
 			$.fn.makePaneNormal();
 		});
 		
+		//pane
 		$('.pane-handler').bind('click',function(){
 			$.fn.removeAllActive.call(this);
+			var id = $(this).attr('data-aim');
+			$('.pane').not('#'+ id).removeClass('show');
+			
+			if($('#'+ id).hasClass('show')){
+				$('#'+ id).removeClass('show');
+			}else{
+				$('#'+ id).css('right','').addClass('show');
+			}
 		});
 		
 	});
