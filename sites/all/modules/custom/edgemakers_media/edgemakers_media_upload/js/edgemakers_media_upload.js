aea51331095f35728f3e5859016ca1d6d9ffebf6
lugal-cn <lugal.cn@gmail.com>
2013-08-14 03:03:18 +0800

    #Load media list after document ready, and bind click same with set item in js; #Load media list HTML code in module file.

---
+++ sites/all/modules/custom/edgemakers_media/edgemakers_media_upload/js/edgemakers_media_upload.js
@@ -0,0 +1,44 @@
+
+jQuery(document).ready(function(){
+  media_ajax_load_list();
+});
+
+function media_ajax_load_list() {
+  jQuery('#my-media-list').load("?q=edgemarkers/media/get/list/ajax", function(){
+
+    jQuery('ul#media-list li a').each(function( index ) {
+
+      var nid = jQuery(this).attr('id').substring(11);
+      //alert("Node nid:" + nid);
+      var ajaxUrl = '?q=edgemakers/stage/api/set/info/ajax/' + nid;
+      var ajaxContent;
+      
+      jQuery(this).bind('click', function(){
+
+        //openAjaxLoad("ajaxload");
+        
+        jQuery.ajax({
+          url: ajaxUrl,
+          type: "GET",
+          success: function(data) {
+
+            jQuery("#stage-set-view").html(data);
+            jQuery("#set-view-region").show();
+            jQuery("#stage-set-list").hide();
+            jQuery("#back-set-list").show();
+            
+            return false;
+
+          },
+          error :function(){
+            jQuery("#stage-set-view").html("Load set data error.");
+            return false;
+          }
+        });
+        
+        return false;
+
+      });
+    });
+  });
+}
