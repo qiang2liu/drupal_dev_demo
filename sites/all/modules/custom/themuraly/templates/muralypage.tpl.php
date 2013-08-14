f204dba5906c6bd1b74e37554cc34274e16ffb9b
lugal-cn <lugal.cn@gmail.com>
2013-08-14 20:24:42 +0800

    #Set mural iframe height.

--- sites/all/modules/custom/themuraly/templates/muralypage.tpl.php
+++ sites/all/modules/custom/themuraly/templates/muralypage.tpl.php
@@ -9,6 +9,6 @@ $the_m_id = $data['m_id'];
 
 
 <div>
-<iframe id="mural-ly-iframe" frameborder='0' src='http://staging.mural.ly/embed/edgemakers/edgemakers/<?php print $the_m_id;?>' width=100% height=auto'></iframe>
+<iframe id="mural-ly-iframe" frameborder='0' src='http://staging.mural.ly/embed/edgemakers/edgemakers/<?php print $the_m_id;?>' width=100% height=600'></iframe>
 
 </div>
