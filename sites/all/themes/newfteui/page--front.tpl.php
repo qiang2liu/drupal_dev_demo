f254f5c3b9d72af3deab0fd8e9b5bbe98f6cce19
tao2.guo <tao2.guo@symbio.com>
2013-08-14 13:32:32 +0800

    FTE-273

--- sites/all/themes/newfteui/page--front.tpl.php
+++ sites/all/themes/newfteui/page--front.tpl.php
@@ -91,7 +91,7 @@
 <div class="sliding-panes">
   <div class="studio-handler pane-handler" data-aim="studio"></div>
   <div class="chanllenge-handler pane-handler" data-aim="challenge"></div>
-  <!--<div class="competitions-handler pane-handler"  data-aim="competitions"></div>-->
+  <div class="competitions-handler pane-handler"  data-aim="competitions"></div>
   <?php print render($page['sliding_panes']); ?>
 </div>
 <div class="pane" id="studio">
@@ -106,7 +106,7 @@
 
 </div>
 <div class="pane" id="challenge"></div>
-<!--<div class="pane" id="competitions"></div>-->
+<div class="pane" id="competitions"></div>
 
 <!-- / user profile-->
 <div class="user-profile">
