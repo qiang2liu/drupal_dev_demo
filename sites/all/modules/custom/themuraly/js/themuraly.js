f29cd13f9fba6983905f95d247cbad8d9aea23bb
lugal-cn <lugal.cn@gmail.com>
2013-08-14 14:00:33 +0800

    #Add mural module js/css.

---
+++ sites/all/modules/custom/themuraly/js/themuraly.js
@@ -0,0 +1,45 @@
+
+jQuery(document).ready(function(){
+  
+  jQuery(".toolbar-item.add-an-idea").show();
+  jQuery(".create-mural").show();
+  jQuery(".create-mural").bind("click", function(){
+
+    //bigFrame.attr('src', source);
+    if (jQuery("#mural-region").length !== 0) {
+      
+      var source = jQuery(this).attr("href");
+
+      //jQuery("#stage-set-list").hide();
+      
+      var iframeHtml = "<iframe id=\"mural-iframe\" width=\"100%\" height=\"80%\" src=\"?q=mural/create\"></iframe>";
+      
+      //jQuery("#mural-region").html("Mural display here by iframe " + source);
+      jQuery("#mural-iframe").attr("src", source);
+      jQuery("#mural-iframe").attr("width", jQuery(window).width());
+      jQuery("#mural-iframe").attr("height", jQuery(window).height() - 90);
+      
+      jQuery( "#mural-region" ).dialog({
+        resizable: false,
+        modal: true,
+        position: ["left", "top"],
+        width: "99%",
+        height: jQuery(document).height() + 50,
+        zIndex: 1000,
+        /*buttons: {
+          Ok: function() {
+            jQuery(this).dialog( "close" );
+          }
+        }*/
+      });
+      
+      //jQuery('.ui-dialog-titlebar').attr("style", "display: none;");
+      jQuery('.ui-dialog-titlebar').attr("style", "height: 21px;");
+      //jQuery(".ui-dialog").attr("style", "padding-left: 1px;");
+      //jQuery("#mural-region").show();
+
+    }
+    //alert("Hook create mural link. and should set iframe src to " + source);
+    return false;
+  });
+});
