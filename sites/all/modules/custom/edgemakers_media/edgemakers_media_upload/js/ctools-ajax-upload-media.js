6e2a19f3330b0fc362897188d915db625da98f88
tao2.guo <tao2.guo@symbio.com>
2013-08-21 15:13:54 +0800

    FTE-277

--- sites/all/modules/custom/edgemakers_media/edgemakers_media_upload/js/ctools-ajax-upload-media.js
+++ sites/all/modules/custom/edgemakers_media/edgemakers_media_upload/js/ctools-ajax-upload-media.js
@@ -22,7 +22,8 @@ Drupal.theme.prototype.CToolsEdgemakersUploadMediaModal = function () {
   html += '              <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeText + '</a></span>';
   html += '              <div class="clear-block"></div>';
   html += '            </div>';
-  html += '            <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+  html += '            <div class="media-upload-header">Media Upload</div>'
+  html += '            <div class="modal-scroll"><div class="media-upload-close popups-close"><a class="close" href="#">&nbsp;</a></div><div id="modal-content" class="modal-content popups-body"></div></div>';
   html += '            <div class="popups-buttons"></div>'; //Maybe someday add the option for some specific buttons.
   html += '            <div class="popups-footer"></div>'; //Maybe someday add some footer.
   html += '          </div>';
