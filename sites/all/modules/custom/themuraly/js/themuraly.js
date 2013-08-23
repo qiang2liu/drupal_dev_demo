282c9a05c8de1266f535f68c44b32ebac71f2297
lugal-cn <lugal.cn@gmail.com>
2013-08-23 14:14:07 +0800

    #Fix studio mural list link click.

--- sites/all/modules/custom/themuraly/js/themuraly.js
+++ sites/all/modules/custom/themuraly/js/themuraly.js
@@ -121,54 +121,72 @@ function mural_ajax_load_list() {
   });
 }
 
+// Studio/Gallery mural list json
+//   @type my[idea], share[with me], ideas
+function studio_mural_ajax_page_load(type, pager) {
+
+  jQuery.ajax({
+    url: "?q=mural/studio/list/page/ajax/" + type + "/" +pager,
+    type: "JSON",
+    success: (function(data){
+      console.log(data);
+      studio_mural_item_bind_link();
+      //@TODO GT change mural list.
+    }),
+  });
+  
+}
+
+function studio_mural_item_bind_link() {
+  jQuery('ul#studio-mural-list li a ').each(function( index ) {
+
+    //alert("Hook mural list link.");
+    jQuery(this).bind('click', function(){
+
+      //alert("click mural link.");
+      if (jQuery("#mural-region").length !== 0) {
+        var source = jQuery(this).attr("href");
+        showMuralDialog(source);
+      }
+      
+      return false;
+
+    });
+  });
+  
+  jQuery('ul#studio-mural-list li a.studio-mural-settings').each(function( index ) {
+  
+    jQuery(this).unbind("click");
+
+    //alert("Hook mural list link.");
+    jQuery(this).bind('click', function(){
+
+      alert("click mural link.");
+
+      return false;
+
+    });
+  });
+}
 
 function studio_mural_ajax_load_list() {
 
   jQuery("#studio-mural-list").load("?q=mural/studio/get/list/ajax", function(){
-
-    jQuery('.toolbar-item').each(function(){
-      var self = jQuery(this);
-      
-      if(self.children().first().hasClass('has-child')){
-        self.find('h4>em').bind('click', function(){
-          if(!self.find('.item-list').hasClass('active')){
-            jQuery('.toolbar-item').find('.item-list').removeClass('active');
-            self.find('.item-list').addClass('active')
-          }
-        })
-      }
-    });
-
-    jQuery('ul#mural-list li a ').each(function( index ) {
-
-      //alert("Hook mural list link.");
-      jQuery(this).bind('click', function(){
-
-        //alert("click mural link.");
-        if (jQuery("#mural-region").length !== 0) {
-          var source = jQuery(this).attr("href");
-          showMuralDialog(source);
-        }
-        
-        return false;
-
-      });
+    
+    jQuery("#studio-my-idea .prev").bind("click", function(){
+      var pager = jQuery(this).find("a").attr("id").substring(5);
+//      alert("Prev page: " + pager);
+      studio_mural_ajax_page_load('my', pager);
+      return false;
     });
     
-    jQuery('ul#mural-list li a.studio-mural-settings').each(function( index ) {
-    
-      jQuery(this).unbind("click");
-  
-      //alert("Hook mural list link.");
-      jQuery(this).bind('click', function(){
-  
-        alert("click mural link.");
-  
-        return false;
-  
-      });
+    jQuery("#studio-my-idea .next").bind("click", function(){
+      var pager = jQuery(this).find("a").attr("id").substring(5);
+//      alert("Next page: " + pager);
+      studio_mural_ajax_page_load('my', pager);
+      return false;
     });
-
-      
+          
   });
+  
 }
