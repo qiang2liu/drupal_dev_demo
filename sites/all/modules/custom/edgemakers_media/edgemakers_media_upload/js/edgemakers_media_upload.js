8b452ec39fad5f6ba7cc80db397e920e3d823a10
lugal-cn <lugal.cn@gmail.com>
2013-08-28 18:44:08 +0800

    #Add video/image list on gallery pane; #Fix mural list cover style on gallery.

--- sites/all/modules/custom/edgemakers_media/edgemakers_media_upload/js/edgemakers_media_upload.js
+++ sites/all/modules/custom/edgemakers_media/edgemakers_media_upload/js/edgemakers_media_upload.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
   media_ajax_load_list();
   studio_media_list_ajax_load();
+  gallery_media_list_ajax_load('video', 0);
+  gallery_media_list_ajax_load('image', 0);
   //Drupal.behaviors.mediaListAjaxload();
 });
 
@@ -90,80 +92,62 @@ function studio_media_list_ajax_load(pager) {
         alert("data is empty");
       }
       else {
-//        alert(data.length);
         jQuery("#studio-media-list").html(data);
         
         jQuery('ul#studio-media-list li a').each(function( index ) {
           var nid = jQuery(this).attr('nid');
           var title = jQuery(this).html();
-
           jQuery(this).unbind("click");
           jQuery(this).bind("click", function(){
             showMediaOnDestination(nid, title);
             return false;
           });
         });
-        
-        reBindLeftRight(pager);
-        
-        
+        studioBindLeftRight(pager);
       }
-      
-    }
-    
-  });
-  
-  /*jQuery("#studio-media-list").load("?q=edgemarkers/studio/media/get/list/ajax/" + pager, function(data){
-    
-    //console.log(data);
-    console.log(pager);
-    if (data.length !== 0) {
-      
-      jQuery('ul#studio-media-list li a').each(function( index ) {
-        var nid = jQuery(this).attr('nid');
-        var title = jQuery(this).html();
-        
-        jQuery(this).unbind("click");
-        jQuery(this).bind("click", function(){
-          showMediaOnDestination(nid, title);
-          return false;
-        });
-      });
-      
-      if (pager == 0) {
-        var prevPager = 0;
-        var nextPager = 1;
-      }
-      else {
-        var prevPager = parseInt(pager) - 1;
-        var nextPager = parseInt(pager) + 1;
-      }
-      
-      jQuery("#studio-media-list-pane .scroll-wrapper .arrow-left").attr("pager", prevPager);
-      jQuery("#studio-media-list-pane .scroll-wrapper .arrow-right").attr("pager", nextPager);
-      
-      jQuery("#studio-media-list-pane .arrow-left").unbind("click");
-      jQuery("#studio-media-list-pane .arrow-left").bind("click", function(){
-        var pager = jQuery(this).attr("pager");
-        studio_media_list_ajax_load(pager);
-        return false;
-      });
-      
-      jQuery("#studio-media-list-pane .arrow-right").unbind("click");
-      jQuery("#studio-media-list-pane .arrow-right").bind("click", function(){
-        var pager = jQuery(this).attr("pager");
-        studio_media_list_ajax_load(pager);
-        return false;
-      });
-
     }
   });
-  */
- 
   
 }
 
-function reBindLeftRight(pager) {
+function gallery_media_list_ajax_load(type, pager) {
+  
+  if (type == null) {
+    return false;
+  }
+
+  if (pager == null) {
+    pager = 0;
+  }
+  
+  jQuery.ajax({
+    url: "?q=edgemarkers/gallery/media/get/list/ajax/" + type + "/" + pager,
+    dataType: 'html',
+    type : 'GET',
+    success : function(data){
+      if (data.length === 0) {
+        console.log("data is empty");
+      }
+      else {
+        jQuery("#gallery-video-list").html(data);
+        
+        jQuery('ul#gallery-video-list li a').each(function( index ) {
+          var nid = jQuery(this).attr('nid');
+          var title = jQuery(this).html();
+          jQuery(this).unbind("click");
+          jQuery(this).bind("click", function(){
+            showMediaOnDestination(nid, title);
+            return false;
+          });
+        });
+        galleryBindLeftRight(type, pager);
+      }
+    }
+  });
+  
+}
+
+function studioBindLeftRight(pager) {
   
   if (pager == 0) {
     var prevPager = 0;
@@ -190,5 +174,37 @@ function reBindLeftRight(pager) {
     studio_media_list_ajax_load(pager);
     return false;
   });
+
+}
+
+function galleryBindLeftRight(type, pager) {
+  if (pager == 0) {
+    var prevPager = 0;
+    var nextPager = 1;
+  }
+  else {
+    var prevPager = parseInt(pager) - 1;
+    var nextPager = parseInt(pager) + 1;
+  }
   
+  var arrowElement;
+  arrowElement = 'gallery-' + type + '-list-pane';
+  alert("arrowElement load data.");
+    
+  jQuery("#" + arrowElement + " .scroll-wrapper .arrow-left").attr("pager", prevPager);
+  jQuery("#" + arrowElement + " .scroll-wrapper .arrow-right").attr("pager", nextPager);
+  
+  jQuery("#" + arrowElement + " .arrow-left").unbind("click");
+  jQuery("#" + arrowElement + " .arrow-left").bind("click", function(){
+    var pager = jQuery(this).attr("pager");
+    gallery_media_list_ajax_load(type, pager);
+    return false;
+  });
+  
+  jQuery("#" + arrowElement + " .arrow-right").unbind("click");
+  jQuery("#" + arrowElement + " .arrow-right").bind("click", function(){
+    var pager = jQuery(this).attr("pager");
+    gallery_media_list_ajax_load(type, pager);
+    return false;
+  });
 }
