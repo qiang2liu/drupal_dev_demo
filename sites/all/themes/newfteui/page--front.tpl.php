7a0251ba7793a98601ed8fef3675782dadf47198
yangru <yangru@ymail.com>
2013-08-30 16:38:17 +0800

    FTE-11

--- sites/all/themes/newfteui/page--front.tpl.php
+++ sites/all/themes/newfteui/page--front.tpl.php
@@ -129,7 +129,7 @@
   <?php
   else :
     $mural_link = 'modal_forms/nojs/login';
-  	$link_class = array('ctools-use-modal',  'ctools-modal-modal-popup-small');
+  	$link_class = array('ctools-use-modal',  'ctools-modal-modal-popup-profilesetting');
   	$login_link = l('<div class="studio-handler pane-handler"></div>', $mural_link , array(
     		'attributes' => array(
     			'class' => $link_class,
