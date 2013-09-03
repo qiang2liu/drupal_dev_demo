ffb93c5234f660ae23512351b330d1a6685dab29
tao2.guo <tao2.guo@symbio.com>
2013-09-03 14:33:54 +0800

    setting-title

--- sites/all/modules/custom/modal_forms/js/modal_forms_popup.js
+++ sites/all/modules/custom/modal_forms/js/modal_forms_popup.js
@@ -84,7 +84,7 @@ Drupal.theme.prototype.ModalFormsProfileSetting = function () {
   html += '  <div class="ctools-modal-content modal-forms-modal-content">';
   html += '    <div class="popups-container">';
   html += '      <div class="modal-header popups-title">';
-  html += '        <span id="modal-title" class="modal-title"></span>';
+  html += '        <span id="modal-title" class="modal-title setting-title"></span>';
   html += '        <span class="popups-close close">' + Drupal.CTools.Modal.currentSettings.closeText + '</span>';
   html += '        <div class="clear-block"></div>';
   html += '      </div>';
