b0f0326817058dfae2202918fd142620158f68c1
lugal-cn <lugal.cn@gmail.com>
2013-09-12 13:47:48 +0800

    #FTE-365

--- sites/all/modules/custom/themuraly/js/refreshElements.js
+++ sites/all/modules/custom/themuraly/js/refreshElements.js
@@ -1,27 +1,42 @@
+(function ($) {
 
-Drupal.CTools.AJAX.refreshElements = function() {
-  var url = '?q=/mural/ajax/refresh-elements';
+Drupal.behaviors.muralRefreshAjax = {
+  attach: function (context){
+
+    changeTitleAfterSetting();
   
-  try {
-    jQuery.ajax({
-      type: "POST",
-      url: url,
-      data: { 'js': 1, 'ctools_ajax': 1},
-      global: true,
-      success: Drupal.CTools.AJAX.respond,
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        alert("Status: " + textStatus);
-//        Drupal.CTools.AJAX.handleErrors(XMLHttpRequest, url);
-      },
-      complete: function() {
-      },
-      dataType: 'json'
-    });
   }
-  catch (err) {
-    alert('An error occurred while attempting to process ' + url);
-    return false;
-  }
- 
-  return false;
-};
+}
+
+})(jQuery);
+
+
+//Drupal.CTools.AJAX.refreshElements = function() {
+//  
+//  var url = '?q=/mural/ajax/refresh-elements';
+//  
+//  try {
+//    jQuery.ajax({
+//      type: "POST",
+//      url: url,
+//      data: { 'js': 1, 'ctools_ajax': 1},
+//      global: true,
+//      success: Drupal.CTools.AJAX.respond,
+//      error: function(XMLHttpRequest, textStatus, errorThrown) {
+//        alert("Status: " + textStatus);
+////        Drupal.CTools.AJAX.handleErrors(XMLHttpRequest, url);
+//      },
+//      complete: function() {
+//      },
+//      dataType: 'json'
+//    });
+//  }
+//  catch (err) {
+//    alert('An error occurred while attempting to process ' + url);
+//    return false;
+//  }
+//  
+//  jQuery("#mural-top-bar #mural-title").addClass("Title changed");
+// 
+//  return false;
+//};
