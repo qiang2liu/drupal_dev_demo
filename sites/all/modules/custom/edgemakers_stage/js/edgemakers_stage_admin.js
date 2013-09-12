7d575546c7fa04e3a7f2fb2eda6b79e53a412219
lugal-cn <lugal.cn@gmail.com>
2013-09-12 15:43:26 +0800

    #FTE-360

--- sites/all/modules/custom/edgemakers_stage/js/edgemakers_stage_admin.js
+++ sites/all/modules/custom/edgemakers_stage/js/edgemakers_stage_admin.js
@@ -6,15 +6,12 @@ function disp_confirm(id) {
   var r=confirm("Are you sure?");
   if (r==true)
   {
-//    alert(id);
     var url = jQuery("#"+id).attr("href");
-//    alert("You pressed OK!" + id);
     window.location = url;
     return true;
   }
   else
     {
-//    alert("You pressed Cancel!");
     return false;
   }
 }
