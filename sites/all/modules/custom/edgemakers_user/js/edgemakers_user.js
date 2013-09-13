300f34a532d1718161f94309a4e4afca2302835b
yangru <yangru@ymail.com>
2013-09-13 14:27:59 +0800

    FTE-325

--- sites/all/modules/custom/edgemakers_user/js/edgemakers_user.js
+++ sites/all/modules/custom/edgemakers_user/js/edgemakers_user.js
@@ -18,10 +18,10 @@ function profileBackground() {
 }
 Drupal.behaviors.fileUpload = {
   attach: function(context, settings) {
-    jQuery("body").ajaxComplete(function(event,request, settings){
+    jQuery("body").ajaxComplete(function(event,request, settings) {
       //if(window.location.pathname.match(/home/)) {
         if (form_build_id = settings.url.match(/file\/ajax\/field_profile_background\/und\/0\/(.*)$/)) {
-          if(jQuery('[value="'+form_build_id[1]+'"]').closest('form').find('[id$=remove-button]').length == 0) {
+          if(jQuery('[value="'+form_build_id[1]+'"]').closest('form').find('[id=edit-field-profile-background-und-0-remove-button]').length == 0) {
             jQuery('.form-item-preset-background label.option').removeClass('active');
             changeBgImage();
           } else {
@@ -30,7 +30,7 @@ Drupal.behaviors.fileUpload = {
               jQuery('.form-item-preset-background input[type=radio]').attr('checked', false);
               changeBgImage(this.src);
             });
-            jQuery('[value="'+form_build_id[1]+'"]').closest('form').find('[id$=remove-button]').bind('click', function(){
+            jQuery('[value="'+form_build_id[1]+'"]').closest('form').find('[id=edit-field-profile-background-und-0-remove-button]').bind('click', function(){
               if(jQuery('.form-item-preset-background label.option.active').length > 0) {
                 changeBgImage(jQuery('.form-item-preset-background label.option.active span.image img')[0].src);
               } else {
@@ -39,8 +39,16 @@ Drupal.behaviors.fileUpload = {
             });
             jQuery('.form-item-field-profile-background-und-0 img')[0].click();
           }
+        } else if (form_build_id = settings.url.match(/file\/ajax\/field_profile_picture\/und\/0\/(.*)$/)) {
+          if(jQuery('[value="'+form_build_id[1]+'"]').closest('form').find('[id=edit-field-profile-picture-und-0-remove-button]').length == 0) {
+            changeProfileImage();
+          } else {
+            if(jQuery('.form-item-field-profile-picture-und-0 img').length > 0) {
+              changeProfileImage(jQuery('.form-item-field-profile-picture-und-0 img')[0].src);
+            }
+          }
         }
-      //}  
+    //}  
     });
   }
 };
@@ -60,6 +68,16 @@ Drupal.behaviors.autoUpload = {
     });
   }
 };
+function changeProfileImage(src) {
+  var imgEl = jQuery('.user-profile-inner img');
+  if(imgEl.length > 0) {
+    if(src) {
+      imgEl[0].src = src;
+    } else {
+      imgEl[0].src = '/sites/default/files/styles/thumbnail/public/default_avatar.png';
+    }
+  }
+}
 function changeBgImage(src) {
   if(src) {
     src = src.replace(/styles\/[^\/]*\/public\//, '');
