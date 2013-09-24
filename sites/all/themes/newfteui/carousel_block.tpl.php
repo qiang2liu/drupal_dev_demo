dcd73a649e5284af6402f37fee08ee425266d4a0
yangru <yangru@ymail.com>
2013-09-24 17:44:07 +0800

    FTE-440

--- sites/all/themes/newfteui/carousel_block.tpl.php
+++ sites/all/themes/newfteui/carousel_block.tpl.php
@@ -1,22 +1,11 @@
 <div id="d3">
 	<div class="d3-left-arrow"><img  src="<?php print drupal_get_path('theme', 'newfteui');?>/images/ico_da_slider_left.png"  /></div>
-		<?php foreach($items as $item):
-      if($item['topicid']) {
-        $topic = node_load($item['topicid']);
-        $title = $topic->title;
-        $descriptions = field_get_items('node', $topic, 'field_topic_problem');
-        $description = $descriptions && count($descriptions) ? $descriptions[0]['value'] : '';
-        $description = text_summary($description, NULL, 185);
-      } else {
-        $title = '';
-        $description = '';
-      }
-    ?>
+		<?php foreach($items as $item): ?>
     <div class="d3-item">
       <img src="<?php echo $item['imageurl'];?>" />
       <div class="carousel-des">
-        <h5><?php echo $title;?></h5>
-        <p><?php echo $description;?></p>
+        <h5><?php echo $item['title'];?></h5>
+        <p><?php echo $item['description'];?></p>
         <div class="carousel-viewmore">
           <a href="node/<?php echo $item['topicid'];?>" target="_blank">View Wicked Problem</a>
         </div>
