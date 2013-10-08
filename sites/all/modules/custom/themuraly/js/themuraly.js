d08125d6845e35f97dde0788c454d46d9b991c10
tao2.guo <tao2.guo@symbio.com>
2013-10-08 13:20:43 +0800

    ui

--- sites/all/modules/custom/themuraly/js/themuraly.js
+++ sites/all/modules/custom/themuraly/js/themuraly.js
@@ -93,7 +93,7 @@ function setMuralWidth(){
   jQuery("#mural-iframe").attr("width", jQuery(window).width() + 'px');
   if(jQuery('#mural-region').attr('data-user') === '1'){
   	
-  	jQuery("#mural-iframe").attr("height", jQuery(window).height()-29 + 'px');
+  	//jQuery("#mural-iframe").attr("height", jQuery(window).height()-29 + 'px');
   	jQuery('#mural-back-to-dashboard').css('top','34px');
   }else{
   	 jQuery("#mural-iframe").attr("height", jQuery(window).height() + 'px');
