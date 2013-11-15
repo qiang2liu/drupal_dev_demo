da2513b5e58ee545c31046914b41257623f3a6de
yangru <yangru@ymail.com>
2013-11-15 13:39:54 +0800

    FTE-11

--- sites/all/themes/newfteui/templates/page/demo_item.tpl.php
+++ sites/all/themes/newfteui/templates/page/demo_item.tpl.php
@@ -7,11 +7,15 @@
   width: 100%;
   height: 100%;
 }
+.demo_cont {
+  width: 815px;
+  margin: 0 auto;
+}
 </style>
 <div class="compration-test" style="width:100%; background:#fff; text-align:center;">
    <img src = '<?echo $base_url;?>/sites/all/themes/newfteui/images/header.png' />
    <div>
-   <div><?php echo l('< Back', 'demo');?></div>
+   <div class="demo_cont"><?php echo l('< Back', 'demo');?></div>
    <?php echo drupal_render(node_view($item, 'full'));?>
    </div>
 </div>
