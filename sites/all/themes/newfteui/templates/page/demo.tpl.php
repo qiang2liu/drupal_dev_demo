23e7e041ba7b995d1bff4a046c9b97a67b36e6d2
yangru <yangru@ymail.com>
2013-11-15 13:34:52 +0800

    FTE-11

--- sites/all/themes/newfteui/templates/page/demo.tpl.php
+++ sites/all/themes/newfteui/templates/page/demo.tpl.php
@@ -10,5 +10,5 @@
 </style>
 <div class="compration-test" style="width:100%; background:#fff; text-align:center;">
    <img src = 'sites/all/themes/newfteui/images/header.png' />
-   <?php echo theme('image_set_item_demo', array('item' => $item));?>
+   <div><?php echo theme('image_set_item_demo', array('item' => $item));?></div>
 </div>
--- sites/all/themes/newfteui/templates/page/demo_item.tpl.php
+++ sites/all/themes/newfteui/templates/page/demo_item.tpl.php
@@ -10,6 +10,8 @@
 </style>
 <div class="compration-test" style="width:100%; background:#fff; text-align:center;">
    <img src = '<?echo $base_url;?>/sites/all/themes/newfteui/images/header.png' />
-   <?php echo l('< Back', 'demo');?>
+   <div>
+   <div><?php echo l('< Back', 'demo');?></div>
    <?php echo drupal_render(node_view($item, 'full'));?>
+   </div>
 </div>
